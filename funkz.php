53e1db8677ce135bd0ea6ab331398edb9cf709d4
Roy Bellingan <roy@tech.techadsmedia.com>
2021-01-13 17:23:52 +0000

    varie cose

--- funkz.php
+++ funkz.php
@@ -1,77 +1,82 @@
 <?php
 
-require_once( __DIR__ . '/config.php' );
-
 /** Heredoc Hack
  * @param $data
  * @return mixed
  */
-$fn = function( $data ){ return $data; };
+$fn = function ($data) {
+    return $data;
+};
 
-function request_ifset( string $what , bool $numeric = false , $default = 0 ) : ?string {
-    $db = DBS7();
+function request_ifset(string $what, bool $numeric = false, $default = NULL): ?string
+{
 
-    if( $numeric && ! isset( $_REQUEST[$what] ) ){
-        return $db->escape( $default );
+    if ($numeric && !isset($_REQUEST[$what])) {
+        return $default;
     }
 
-    if( $numeric && ! is_numeric( $_REQUEST[$what] ) ){
-        return $db->escape( $default );
+    if ($numeric && !is_numeric($_REQUEST[$what])) {
+        return $default;
     }
 
-    if( isset( $_REQUEST[$what] ) ){
-        if( $_REQUEST[$what] === 0 || $_REQUEST[$what] == '0' ){
-            return ( int ) 0;
-        }else{
-            return $db->escape( $_REQUEST[$what] );
+    if (isset($_REQUEST[$what])) {
+        if ($_REQUEST[$what] === 0 || $_REQUEST[$what] == '0') {
+            return ( int )0;
+        } else {
+            return $_REQUEST[$what];
         }
     }
 
-    if( $default ){
-        return $db->escape( $default );
+    if ($default) {
+        return $default;
     }
 
     return false;
 }
 
-function request_required( string $what , bool $numeric = false ) : string {
-    $res = request_ifset( $what , $numeric , false );
-    if( ! $res ){
-        die( "Missing required parameter: {$what}" );
+function request_required(string $what, bool $numeric = false): string
+{
+    $res = request_ifset($what, $numeric, false);
+    if (!$res) {
+        die("Missing required parameter: {$what}");
     }
     return $res;
 }
 
-function request_requiredMulti( array $elements ) : array {
+function request_requiredMulti(array $elements): array
+{
     $values = array();
-    foreach( $elements as $element ){
-        $values[$element["what"]] = request_required( $element["what"] , $element["numeric"] );
+    foreach ($elements as $element) {
+        $values[$element["what"]] = request_required($element["what"], $element["numeric"]);
     }
     return $values;
 }
 
-function getRequestIp() : string {
-    return request_ifset( 'ip' , false , getIp() );
+function getRequestIp(): string
+{
+    return request_ifset('ip', false, getIp());
 }
 
-function getIp() : string {
-    if( getenv( 'HTTP_CLIENT_IP' ) ) {
-        $ip = getenv( 'HTTP_CLIENT_IP' );
-    } else if( getenv( 'HTTP_X_FORWARDED_FOR' ) ){
-        $ip = getenv( 'HTTP_X_FORWARDED_FOR' );
-    } else if( getenv( 'HTTP_X_FORWARDED' ) ){
-        $ip = getenv( 'HTTP_X_FORWARDED' );
-    } else if( getenv( 'HTTP_FORWARDED_FOR' ) ){
-        $ip = getenv( 'HTTP_FORWARDED_FOR' );
-    } else if( getenv( 'HTTP_FORWARDED' ) ){
-        $ip = getenv( 'HTTP_FORWARDED' );
-    } else if( getenv( 'REMOTE_ADDR' ) ){
-        $ip = getenv( 'REMOTE_ADDR' );
+function getIp(): string
+{
+    if (getenv('HTTP_CLIENT_IP')) {
+        $ip = getenv('HTTP_CLIENT_IP');
+    } else if (getenv('HTTP_X_FORWARDED_FOR')) {
+        $ip = getenv('HTTP_X_FORWARDED_FOR');
+    } else if (getenv('HTTP_X_FORWARDED')) {
+        $ip = getenv('HTTP_X_FORWARDED');
+    } else if (getenv('HTTP_FORWARDED_FOR')) {
+        $ip = getenv('HTTP_FORWARDED_FOR');
+    } else if (getenv('HTTP_FORWARDED')) {
+        $ip = getenv('HTTP_FORWARDED');
+    } else if (getenv('REMOTE_ADDR')) {
+        $ip = getenv('REMOTE_ADDR');
     } else {
         $ip = '';
     }
 }
 
-function getUserAgent() : string {
+function getUserAgent(): string
+{
     return $_SERVER["HTTP_USER_AGENT"] ?? '';
 }
