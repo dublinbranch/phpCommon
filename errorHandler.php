cb6f498d58a5269b15c4e994f6152be3a34e3bbb
Ivano Mercuri <ivano.mercuri@tech.techadsmedia.com>
2021-03-18 16:03:00 +0100

    filter some errors

--- errorHandler.php
+++ errorHandler.php
@@ -1,48 +1,59 @@
 <?php
-	//ASSOLUTAMENTE QUESTO FILE DEVE ESSERE STAND ALONE, OGIN DIPENDENZA PUÒ ESSER BUGGA E SCASSARLO
+//ASSOLUTAMENTE QUESTO FILE DEVE ESSERE STAND ALONE, OGIN DIPENDENZA PUÒ ESSER BUGGA E SCASSARLO
 
-    function sendToSlack(string $txt, $config): void
-    {
-        if (strlen($txt) == 0) {
-            return;
-        }
-	$data = array(
-	"text" => $config->who . " " . date("Y-m-dTH:i:s") . " {$txt}\n",
-	"channel" => $config->channel,
-	"username" => $config->username
-	);
-	$curl = curl_init( $config->url);
-	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	$a = curl_exec($curl);
-	curl_close($curl);
+function sendToSlack(string $txt, object $config): void
+{
+    if (strlen($txt) == 0) {
+        return;
     }
+    $data = array(
+        "text" => $config->who . " " . date("Y-m-dTH:i:s") . " {$txt}\n",
+        "channel" => $config->channel,
+        "username" => $config->username
+    );
+    $curl = curl_init($config->url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($curl);
+    curl_close($curl);
+}
 
-register_shutdown_function(function () {
-    $error = error_get_last();
-    /*
-    filrtare via 
-                            "pattern" : [
-                        "PHP Warning:  file_get_contents(https://www.bingapis.com/api/V7/ads/adsimpressionfeedback"
-                        ,"PHP Warning:  file_get_contents(): Failed to enable crypto"
-                        ,"PHP Warning:  file_get_contents(): SSL: Connection reset by peer in"
-                        ,""
-                        ]
+function errorFilter(&$errorMessage): void
+{
+    $excludeds = array(
+        "file_get_contents(https://www.bingapis.com/api/V7/ads/adsimpressionfeedback",
+        "file_get_contents(): Failed to enable crypto",
+        "file_get_contents(): SSL: Connection reset by peer in"
+    );
+    foreach ($excludeds as $excluded) {
+        if (stripos($errorMessage, $excluded) !== false) {
+            $errorMessage = "";
+            break;
+        }
+    }
+}
 
-    */
-    if (isset($error["type"])){ // && ($error["type"] == E_ERROR || $error["type"] == E_COMPILE_ERROR || $error["type"] == 4) ) {
-	$config = json_decode(file_get_contents("/srv/www/phpCommon/config.json"));
+function handleError(?array $error): void
+{
+    if (isset($error["type"])) {
+        $config = json_decode(file_get_contents(__DIR__ . "/config.json"));
         if ($config->active) {
+            errorFilter($error["message"]);
             $key = 'errorTrackerTS';
             $lastTS = apcu_fetch($key);
             $difference = time() - $lastTS;
-            if (!$lastTS || $difference > $config->TTL) {
+            if (strlen($error["message"]) > 0 && (!$lastTS || $difference > $config->TTL)) {
                 apcu_store($key, time());
-		$f = $_SERVER["SCRIPT_FILENAME"];
+                $f = $_SERVER["SCRIPT_FILENAME"];
                 mail($config->mail, "FATAL PHP ERROR", "calling $f " . $error["message"]);
-                sendToSlack("calling $f " .  $error["message"], $config);
+                sendToSlack("calling $f " . $error["message"], $config);
             }
         }
     }
+}
+
+register_shutdown_function(function () {
+    $error = error_get_last();
+    handleError($error);
 });
