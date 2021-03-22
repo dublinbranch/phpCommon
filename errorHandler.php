dc358edd2f0fb58801205c2ddc7940fb142ee3ff
Roy Bellinga (s14) <roy@tech.techadsmedia.com>
2021-03-22 14:33:45 +0000

    error refiner

--- errorHandler.php
+++ errorHandler.php
@@ -19,20 +19,22 @@ function sendToSlack(string $txt, object $config): void
     curl_close($curl);
 }
 
-function errorFilter(&$errorMessage): void
+function invalidError($errorMessage): bool
 {
     $excludeds = array(
         "file_get_contents(https://www.bingapis.com/api/V7/ads/adsimpressionfeedback",
         "file_get_contents(): Failed to enable crypto",
         "file_get_contents(): SSL: Connection reset by peer in",
-        "file_get_contents(https://www.bingapis.com/api/ping"
+        "file_get_contents(https://www.bingapis.com/api/ping",
+	"pinocchio/geppetto",
+	"https://askprivate.com/postback?cid=CLICKID"
     );
     foreach ($excludeds as $excluded) {
         if (stripos($errorMessage, $excluded) !== false) {
-            $errorMessage = "";
-            break;
+		return true;
         }
     }
+    return false;
 }
 
 function handleError(?array $error): void
@@ -40,14 +42,22 @@ function handleError(?array $error): void
     if (isset($error["type"])) {
         $config = json_decode(file_get_contents(__DIR__ . "/config.json"));
         if ($config->active) {
-            errorFilter($error["message"]);
+            
+	    if(invalidError($error["message"])){
+		return;
+	    }
+	    if(invalidError($_SERVER["SCRIPT_FILENAME"])){
+		return;
+	    }
+	    
             $key = 'errorTrackerTS';
             $lastTS = apcu_fetch($key);
             $difference = time() - $lastTS;
             if (strlen($error["message"]) > 0 && (!$lastTS || $difference > $config->TTL)) {
                 apcu_store($key, time());
                 $f = $_SERVER["SCRIPT_FILENAME"];
-		$msg = "{$error['message']} in {$error['file']}:{$error['line']}";
+		$ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : NULL;
+		$msg = "{$error['message']} in {$error['file']}:{$error['line']}, refurl: $ref";
                 mail($config->mail, "FATAL PHP ERROR", "calling $f " . $msg);
                 sendToSlack("calling $f " . $msg, $config);
             }
