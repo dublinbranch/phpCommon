0fdafa122e14f71627d66a750f682bee03ee593d
Roy Bellingan <admin@seisho.us>
2021-03-29 18:23:28 +0100

    Update fLock.php

--- fLock.php
+++ fLock.php
@@ -1,14 +1,14 @@
 <?php
 function ensureSingleRun(string $filename) : void
 {
-    global $fp;
-    $fp = fopen(__DIR__ . "/$filename", "c+");
+    //THIS LINE MUST BE KEPT! else the fp will go out of scope and be auto closed!
+    GLOBAL $fp;
+    $file = __DIR__ . "/$filename";
+    $fp = fopen($file, "c+");
     if (!$fp) {
-        die("impossible to open LOCK file");
+        die("impossible to open LOCK file: " . $file);
     }
-
     $locked = flock($fp, LOCK_EX | LOCK_NB);
-
     if (!$locked) {  // acquire an exclusive lock
         die("Couldn't get the lock!");
     }
