9d79d5344279024a4fd78848d7c791319dd0d914
live <live@s18>
2021-04-14 10:17:20 +0000

    multiple forwarding

--- funkz.php
+++ funkz.php
@@ -73,6 +73,10 @@ if (!function_exists("dummyPhpCommonFunkz")) {
         } else {
             $ip = '';
         }
+	//In case of multiple forwarding
+	if(strpos($ip, ",") !== false){
+		return explode(",", $ip)[0];
+	}
         return $ip;
     }
 
