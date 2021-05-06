f3310dbd4c8f337398a0e18d3fa661782ca9f578
Roy Bellingan <roy@tech.techadsmedia.com>
2021-05-06 14:31:21 +0100

    apcSql rischieramento

---
+++ apcSql.php
@@ -0,0 +1,18 @@
+<?php
+if (!function_exists("dummyApcSqlFunkz")) {
+    function dummyApcSqlFunkz()
+    {
+    }
+    //If we are here is guaranteed we already loaded the dbwrapper
+    function apcCached(string $sql, DBWrapper $db, int $ttl = 60)
+    {
+        //we do not store boolean never, so this is ok, at most we have NULL...
+        $res = apcu_fetch($sql);
+        if ($res !== false) {
+            return $res;
+        }
+        $res = $db->getLineSS($sql);
+        apcu_store($sql,$res,$ttl);
+        return $res;
+    }
+}
