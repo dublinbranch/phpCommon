b9f1a3681ff42b8f498fca5b2676ee247025629e
Roy Bellingan <roy@s8.chameleon.ad>
2021-06-09 17:25:56 +0000

    quello che sia

--- logger.php
+++ logger.php
@@ -1,5 +1,12 @@
 <?php
 
 function logger1($path, $msg){
-	file_put_contents($path, date("Y-m-d H:i:s") . "\n" . $msg . "\n--------------------------------------------------------------------------------------------\n", FILE_APPEND | LOCK_EX);
+	$res = @file_put_contents($path, date("Y-m-d H:i:s") . "\n" . $msg . "\n--------------------------------------------------------------------------------------------\n", FILE_APPEND | LOCK_EX);
+	if(!$res){
+        echo "impossible salvare in $path";
+        //$processUser = posix_getpwuid(posix_geteuid());
+        //print_r($processUser);
+	}
+	
+
 }
